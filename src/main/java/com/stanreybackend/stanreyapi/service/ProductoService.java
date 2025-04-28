c607b6c7e425e8473bb98ae6082fb50d9a12e33d
Ana Luz <annateves000@gmail.com>
2025-04-28 11:09:33 -0300

    ABM de Producto nuevo ProductoController con respectivos endpoints

--- src/main/java/com/stanreybackend/stanreyapi/service/ProductoService.java
+++ src/main/java/com/stanreybackend/stanreyapi/service/ProductoService.java
@@ -1,47 +1,83 @@
 package com.stanreybackend.stanreyapi.service;
- 
- import java.util.List;
- 
- import org.springframework.beans.factory.annotation.Autowired;
- import org.springframework.stereotype.Service;
- 
- import com.stanreybackend.stanreyapi.DTO.CategoriaDTO;
- import com.stanreybackend.stanreyapi.DTO.ProductoDTO;
- import com.stanreybackend.stanreyapi.entity.Categoria;
- import com.stanreybackend.stanreyapi.entity.Producto;
- import com.stanreybackend.stanreyapi.repository.CategoriaRepository;
- import com.stanreybackend.stanreyapi.repository.ProductoRepository;
- 
- @Service
- public class ProductoService {
- 
-     @Autowired
-     private ProductoRepository productoRepository;
-     @Autowired
-     private CategoriaRepository categoriaRepository;
- 
-     public String addProducto(ProductoDTO productoDTO) {
- 
-         Producto producto = new Producto(
-             productoDTO.getIdProducto(),
-             productoDTO.getNombre(),
-             productoDTO.getDescripcion(),
-             productoDTO.getPrecio(),
-             productoDTO.getStock(),
-             productoDTO.getUrlImagen(),
-             productoDTO.getColor(),
-             productoDTO.getEstado(),
-             categoriaRepository.findByIdCategoria(productoDTO.getCategoriaId()).orElse(null)
-         );
- 
-         productoRepository.save(producto);
- 
-         return producto.getIdProducto().toString();
- 
-     };
- 
-     public List<Producto> findAll() {
-         List<Producto> productos = productoRepository.findAll();
-         return productos;
-     }
- }
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.stanreybackend.stanreyapi.DTO.ProductoDTO;
+import com.stanreybackend.stanreyapi.entity.Producto;
+import com.stanreybackend.stanreyapi.repository.CategoriaRepository;
+import com.stanreybackend.stanreyapi.repository.ProductoRepository;
+
+@Service
+public class ProductoService {
+
+    @Autowired
+    private ProductoRepository productoRepository;
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    public String addProducto(ProductoDTO productoDTO) {
+
+        Producto producto = new Producto(
+            productoDTO.getIdProducto(),
+            productoDTO.getNombre(),
+            productoDTO.getDescripcion(),
+            productoDTO.getPrecio(),
+            productoDTO.getStock(),
+            productoDTO.getUrlImagen(),
+            productoDTO.getColor(),
+            productoDTO.getEstado(),
+            categoriaRepository.findByIdCategoria(productoDTO.getCategoriaId()).orElse(null)
+        );
+
+        productoRepository.save(producto);
+
+        return producto.getIdProducto().toString();
+
+    };
+
+    public Producto findByIdProducto(Long id_producto) {
+        return productoRepository.findByIdProducto(id_producto).orElse(null);
+    }
+
+    public String bajaByIdproducto(Long id_producto) {
+        Producto producto = findByIdProducto(id_producto);
+        if (producto.getEstado() == 1) {
+            producto.setEstado(0);
+            productoRepository.save(producto);
+        }
+
+        return producto.getIdProducto().toString();
+    }
+
+    public String altaByIdproducto(Long id_producto) {
+        Producto producto = findByIdProducto(id_producto);
+        if (producto.getEstado() == 0) {
+            producto.setEstado(1);
+            productoRepository.save(producto);
+        }
+
+        return producto.getIdProducto().toString();
+    }
+
+    public String updateProducto(Long id_producto, ProductoDTO productoDTO){
+        Producto producto = productoRepository.findByIdProducto(id_producto).orElse(null);
+        producto.setNombre(productoDTO.getNombre());
+        producto.setDescripcion(productoDTO.getDescripcion());
+        producto.setPrecio(productoDTO.getPrecio());
+        producto.setStock(productoDTO.getStock());
+        // producto.setUrlImagen(productoDTO.getUrlImagen());
+        producto.setColor(productoDTO.getColor());
+        producto.setCategoria(categoriaRepository.findByIdCategoria(productoDTO.getCategoriaId()).orElse(null));
+
+        productoRepository.save(producto);
+        return producto.getIdProducto().toString();
+    }
+
+    public List<Producto> findAll() {
+        List<Producto> productos = productoRepository.findAll();
+        return productos;
+    }
+}
